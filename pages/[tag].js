b01dd65cdf8feda582666cea7fd00973270f8ca8
Valon <Saidi>
2022-01-24 03:26:54 +0100

    testing something new

--- pages/[tag].js
+++ pages/[tag].js
@@ -1,26 +1,8 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-// import { fetchByTagName } from "../lib/fetch";
-import { resourcesGroup } from "../data/html";
+import { fetchByTagName } from "../lib/fetch";
 import CardGrid from "../components/Card/CardGrid";
 import styles from "../styles/Tag.module.css";
-
-export async function fetchByTagName(resource) {
-    // const response = await fetch(
-    // "https://resources-next-app.vercel.app/api/getresources"
-    // );
-    const response = await fetch("/api/getresources");
-
-    const data = await response.json();
-    const cardsData = data.data;
-    const filteredData = cardsData.filter((d) => d.resource === resource);
-
-    if (!response.ok) {
-        throw new Error(data.message || "Something went wrong!");
-    }
-
-    return filteredData;
-}
 
 const TagResources = ({ cards }) => {
     const router = useRouter();
@@ -41,9 +23,10 @@ const TagResources = ({ cards }) => {
     );
 };
 
-export async function getServerSideProps({ params }) {
-    const { tag } = params;
-    const cardsData = await fetchByTagName(tag);
+export async function getServerSideProps(context) {
+    const { tag } = context.params;
+    const host = context.req.headers.host;
+    const cardsData = await fetchByTagName(tag, host);
 
     return {
         props: { cards: cardsData },
